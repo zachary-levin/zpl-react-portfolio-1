c6d6f2db8ee69bbd45ea55fb856a81ef50d32ec4
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-05-09 11:34:16 -0500

    Added bio to about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,13 +8,13 @@ export default function About() {
       <h1>About Page</h1>
       <img className='about-subtitle' alt="Me" src={require(`../../images/Nice-pic-of-me.jpg`)} />
       <p>
-        My name is Zachary Levin. (FINISH BIO HERE)
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My name is Zachary Levin. I am a Full-Stack web developer pivoting from a game development background. I have a bachelor’s degree in Media Arts and Game Design from UW-Whitewater. 
+        I recently obtained a certificate in full stack development from Michigan State University, with newly developed skills in JavaScript, Node.js, React.js, and responsive web design. 
+        I'm a problem solver who is always looking to improve and grow, with a focus on mobile-first design and practical applications. 
+        With each project, my aim is to create something that I would want to use myself and is easy to understand for other developers. 
+        I recently worked on a team of four to take an existing README generator project and increased the scale on it. 
+        We created a single-page MERN application focused around the README generator that allows users to login with a username, email, and password to create a professional README. 
+        I am excited to leverage the skills I have gained from my certification to deliver applications that build better experiences for users on the web. 
       </p>
     </div>
   );
