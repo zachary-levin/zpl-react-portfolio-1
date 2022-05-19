8c2cbe3eaf159f5d7cadb48c0f51ec7315f150f1
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-05-18 22:33:21 -0500

    Created Contact component, edited Contact page to contain Contact component

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <div>
+                <h1>
+                    Email:
+                </h1>
+            </div>
+            <div>
+                <h1>
+                    GitHub:
+                </h1>
+            </div>
+            <div>
+                <h1>Resume:</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
